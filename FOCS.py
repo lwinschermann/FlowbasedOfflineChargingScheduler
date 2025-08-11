ad131c42171b4ca293f2f444ccbaffda7f2646cc
Leoni Winschermann <l.winschermann@utwente.nl>
2025-08-11 16:03:39 +0200

    Bugfix
    Make LYNCS compatible with empty instances.

--- FOCS.py
+++ FOCS.py
@@ -558,7 +558,7 @@ class FlowNet:
                         print('[WARNING]: how undefined. Try \'linear\' or \'quadratic\'. No weight assigned.')
                         print('how = ', how)
         #add demands
-        demand = sum([self.G_sched['s']['j'+str(j)]['capacity'] for j in instance.jobs]) - err
+        demand = max(0, sum([self.G_sched['s']['j'+str(j)]['capacity'] for j in instance.jobs]) - err)
         nx.set_node_attributes(self.G_sched, {'s': - demand, 't': demand}, name = 'demand')
 
         return self.G_sched
